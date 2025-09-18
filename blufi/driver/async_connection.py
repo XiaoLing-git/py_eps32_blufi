96de60918a8e6d6d6a31edb3959511ec977cdee1
cwz@xiao <xiaolonghua@wondertechlabs.com>
2025-09-18 14:16:22 +0800

    debug AsyncBlufiConnection

--- blufi/driver/async_connection.py
+++ blufi/driver/async_connection.py
@@ -13,70 +13,70 @@ logger = logging.getLogger(__name__)
 
 
 class AsyncBlufiConnection:
-    """"""
+    """Async Blufi Connection"""
 
     def __init__(self, device_address: str) -> None:
-        """"""
+        """AsyncBlufiConnection init"""
         self.__address = format_mac_address(device_address.strip().upper())
         self.__client: BleakClient | None = None
 
     @property
     def _client(self) -> BleakClient:
-        """给子类调用,当前类不能直接使用"""
+        """only for subclass"""
         if self.__client is None:
-            raise AsyncBlufiConnectionError("设备没有连接")
+            raise AsyncBlufiConnectionError("Device not connected")
         return self.__client
 
-    def del_client(self) -> None:
+    def __del_client(self) -> None:
         """"""
         del self.__client
         self.__client = None
 
     @property
     def address(self) -> str:
-        """"""
+        """get formatted address"""
         return self.__address
 
     async def async_connect(self) -> None:
-        """"""
-        logger.info(f"尝试建立蓝牙连接 address:{self.address}")
+        """async connect"""
+        logger.info(f"Try to establish a Bluetooth connection, address:{self.address}")
         try:
             if self.__client is None:
                 self.__client = BleakClient(self.address)
 
                 await self.__client.connect()
-                logger.info("建立新的连接成功")
+                logger.info("New connection established successfully")
                 return
             if isinstance(self.__client, BleakClient):
                 if self.__client.is_connected:
-                    logger.info("蓝牙已连接，无需再次连接")
+                    logger.info("Bluetooth is already connected, no need to connect again")
                     return
                 else:
-                    logger.info("蓝牙已断开，再次连接中~~~")
+                    logger.info("Bluetooth has been disconnected, reconnecting~~~")
                     await self.__client.connect()
-                    logger.info("蓝牙连接成功")
+                    logger.info("Bluetooth connection successful")
                     return
         except Exception as e:
-            logger.info(f"蓝牙连接过程中异常:{str(e)}")
-            raise AsyncBlufiDisconnectionError(str(e))
+            logger.info(f"Abnormality during Bluetooth connection:{e}")
+            raise AsyncBlufiConnectionError(str(e)) from e
 
     async def async_disconnect(self) -> None:
-        """"""
+        """async disconnect"""
         try:
             if self.__client is None:
-                logger.info("未建立连接，无需断开")
+                logger.info("No connection established, no need to disconnect")
                 return
             if self.__client.is_connected:
-                logger.info("蓝牙已连接，断开中~~~")
+                logger.info("Bluetooth is connected, disconnecting~~~")
                 try:
                     async with as_timeout(0.5):
                         await self.__client.disconnect()
                         await self.__client.unpair()
-                        logger.info("蓝牙已连接，正常断开成功")
+                        logger.info("Bluetooth is connected and disconnected successfully")
                 except asyncio.TimeoutError:
-                    self.del_client()
-                    logger.info("蓝牙已连接，强制断开")
+                    self.__del_client()
+                    logger.info("Bluetooth is connected, force disconnect")
         except Exception as e:
-            msg: str = f"{self.address} 断开连接失败,请尝试重启软件: {e}"
+            msg: str = f"{self.address} Disconnection failed, please try restarting the software: {e}"
             logger.info(msg)
-            raise AsyncBlufiDisconnectionError(msg)
+            raise AsyncBlufiDisconnectionError(msg) from e
