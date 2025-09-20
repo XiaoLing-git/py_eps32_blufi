7bc42c4aa686991b6e74d0e8a7f0b2ccc6ef2278
cwz@xiao <xiaolonghua@wondertechlabs.com>
2025-09-20 19:15:33 +0800

    debug StaWifiSSIDCommand

---
+++ blufi/commands/sta_wifi_bssid_command.py
@@ -0,0 +1,40 @@
+""""""
+
+from ..models.base_models import Ack, CrcCheck, DataAddress, Direction, Encryption, Sector_Data, TypeField
+from ..models.commands_models import ControlCommandWithData, FrameControl, PocketType
+from ..serial_number import SerialNumber
+from ..utils import assert_hex_str
+
+
+class StaWifiBSSIDCommand:
+    """StaWifiBSSIDCommand"""
+
+    __slots__ = ("__cmd",)
+
+    def __init__(
+        self,
+        content: str,
+        encryption: Encryption = Encryption.disable,
+        crc_check: CrcCheck = CrcCheck.disable,
+        direction: Direction = Direction.device_to_esp,
+        ack: Ack = Ack.enable,
+        sector_data: Sector_Data = Sector_Data.disable,
+    ) -> None:
+        """init."""
+        assert_hex_str(content)
+        self.__cmd = ControlCommandWithData(
+            pocket_type=PocketType(type_field=TypeField.Data, func_code=DataAddress.STA_WIFI_BSSID),
+            frame_control=FrameControl(
+                encryption=encryption,
+                crc_check=crc_check,
+                direction=direction,
+                ack=ack,
+                sector_Data=sector_data,
+            ),
+            sn=SerialNumber().obj,
+            data=content,
+        )
+
+    def __str__(self) -> str:
+        """__str__"""
+        return self.__cmd.hex()
