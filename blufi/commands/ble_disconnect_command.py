eb422faf9b838558b181fa25413ad6822db0abb0
cwz@xiao <xiaolonghua@wondertechlabs.com>
2025-09-20 18:53:34 +0800

    add GetWifiListCommand

---
+++ blufi/commands/ble_disconnect_command.py
@@ -0,0 +1,37 @@
+""""""
+
+from ..models.base_models import Ack, ControlAddress, CrcCheck, Direction, Encryption, Sector_Data, TypeField
+from ..models.commands_models import ControlCommand, FrameControl, PocketType
+from ..serial_number import SerialNumber
+
+
+class BleDisconnectCommand:
+    """BleDisconnectCommand"""
+
+    __slots__ = ("__cmd",)
+
+    def __init__(
+        self,
+        encryption: Encryption = Encryption.disable,
+        crc_check: CrcCheck = CrcCheck.disable,
+        direction: Direction = Direction.device_to_esp,
+        ack: Ack = Ack.enable,
+        sector_data: Sector_Data = Sector_Data.disable,
+    ) -> None:
+        """init."""
+
+        self.__cmd = ControlCommand(
+            pocket_type=PocketType(type_field=TypeField.Control, func_code=ControlAddress.CLOSE_CONNECTION),
+            frame_control=FrameControl(
+                encryption=encryption,
+                crc_check=crc_check,
+                direction=direction,
+                ack=ack,
+                sector_Data=sector_data,
+            ),
+            sn=SerialNumber().obj,
+        )
+
+    def __str__(self) -> str:
+        """__str__"""
+        return self.__cmd.hex()
