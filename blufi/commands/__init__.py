1b105e0cc371ac79510975d1f2bcbf29ad1564a6
cwz@xiao <xiaolonghua@wondertechlabs.com>
2025-09-22 11:14:19 +0800

    add  Commands_type

--- blufi/commands/__init__.py
+++ blufi/commands/__init__.py
@@ -1,1 +1,95 @@
 """"""
+
+from typing import Union
+
+from .ack_command import AckCommand
+from .ble_disconnect_command import BleDisconnectCommand
+from .ca_certification_command import CACertificationCommand
+from .client_certification_command import ClientCertificationCommand
+from .client_private_key_command import ClientPrivateKeyCommand
+from .connect_wifi_command import ConnectWifiCommand
+from .custom_command import CustomDataCommand
+from .deauthenticate_command import DeauthenticateCommand
+from .disconnect_wifi_command import DisconnectWifiCommand
+from .get_version_command import GetVersionCommand
+from .get_wifi_list_command import GetWifiListCommand
+from .get_wifi_status_command import GetWifiStatusCommand
+from .negotiate_command import NegotiateCommand
+from .server_certification_command import ServerCertificationCommand
+from .server_private_key_command import ServerPrivateKeyCommand
+from .set_security_mode_command import SetSecurityModeCommand
+from .set_wifi_op_mode_command import SetWifiOpModeCommand
+from .soft_ap_authenticcate_mode_command import SoftApAuthenticateModeCommand
+from .soft_ap_max_connection_command import SoftApMaxConnectionCommand
+from .soft_ap_set_channel_command import SoftApSetChannelCommand
+from .soft_ap_wifi_password_command import SoftApWifiPasswordCommand
+from .soft_ap_wifi_ssid_command import SoftApWifiSSIDCommand
+from .sta_wifi_bssid_command import StaWifiBSSIDCommand
+from .sta_wifi_password_command import StaWifiPasswordCommand
+from .sta_wifi_ssid_command import StaWifiSSIDCommand
+from .wifi_sta_conn_conn_rssi_command import WifiStaConnRssiCommand
+from .wifi_sta_conn_end_reason_command import WifiStaConnEndReasonCommand
+from .wifi_sta_max_conn_retry_command import WifiStaMaxConnRetryCommand
+
+Commands_type = Union[
+    AckCommand,
+    BleDisconnectCommand,
+    CACertificationCommand,
+    ClientCertificationCommand,
+    ClientPrivateKeyCommand,
+    ConnectWifiCommand,
+    CustomDataCommand,
+    DeauthenticateCommand,
+    DisconnectWifiCommand,
+    GetVersionCommand,
+    GetWifiListCommand,
+    GetWifiStatusCommand,
+    NegotiateCommand,
+    ServerCertificationCommand,
+    ServerPrivateKeyCommand,
+    SetSecurityModeCommand,
+    SetWifiOpModeCommand,
+    SoftApAuthenticateModeCommand,
+    SoftApMaxConnectionCommand,
+    SoftApSetChannelCommand,
+    SoftApWifiPasswordCommand,
+    SoftApWifiSSIDCommand,
+    StaWifiBSSIDCommand,
+    StaWifiPasswordCommand,
+    StaWifiSSIDCommand,
+    WifiStaConnRssiCommand,
+    WifiStaConnEndReasonCommand,
+    WifiStaMaxConnRetryCommand,
+]
+
+
+__all__ = (
+    "AckCommand",
+    "BleDisconnectCommand",
+    "CACertificationCommand",
+    "ClientCertificationCommand",
+    "ClientPrivateKeyCommand",
+    "ConnectWifiCommand",
+    "CustomDataCommand",
+    "DeauthenticateCommand",
+    "DisconnectWifiCommand",
+    "GetVersionCommand",
+    "GetWifiListCommand",
+    "GetWifiStatusCommand",
+    "NegotiateCommand",
+    "ServerCertificationCommand",
+    "ServerPrivateKeyCommand",
+    "SetSecurityModeCommand",
+    "SetWifiOpModeCommand",
+    "SoftApAuthenticateModeCommand",
+    "SoftApMaxConnectionCommand",
+    "SoftApSetChannelCommand",
+    "SoftApWifiPasswordCommand",
+    "SoftApWifiSSIDCommand",
+    "StaWifiBSSIDCommand",
+    "StaWifiPasswordCommand",
+    "StaWifiSSIDCommand",
+    "WifiStaConnRssiCommand",
+    "WifiStaConnEndReasonCommand",
+    "WifiStaMaxConnRetryCommand",
+)
