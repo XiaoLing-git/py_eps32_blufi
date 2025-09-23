030a1230f4e25ef9faba6a68847d1a80c68a434f
XiaoLing <samuel.ling.xiao@qq.com>
2025-09-23 15:54:30 +0800

    add WifiStatusParser

--- blufi/responses/wifi_status_parser.py
+++ blufi/responses/wifi_status_parser.py
@@ -1,7 +1,7 @@
 """"""
 
+from ..models.base_models import SoftAPMode, WifiConnectState, WifiOpMode
 from .parser import Parser
-from ..models.base_models import WifiOpMode, SoftAPMode, WifiConnectState
 
 
 class WifiStatusParser(Parser):
@@ -14,32 +14,30 @@ class WifiStatusParser(Parser):
     @property
     def op_mode(self) -> WifiOpMode:
         """op_mode"""
-        return WifiOpMode.map_obj(
-            int.from_bytes(bytes.fromhex(self.content[:2]),byteorder="little"))
+        return WifiOpMode.map_obj(int.from_bytes(bytes.fromhex(self.content[:2]), byteorder="little"))
 
     @property
     def sta_status(self) -> WifiConnectState:
         """op_mode"""
-        return WifiConnectState.map_obj(
-            int.from_bytes(bytes.fromhex(self.content[2:4]), byteorder="little"))
+        return WifiConnectState.map_obj(int.from_bytes(bytes.fromhex(self.content[2:4]), byteorder="little"))
 
     @property
     def soft_ap_status(self) -> SoftAPMode:
         """op_mode"""
-        return SoftAPMode.map_obj(
-            int.from_bytes(bytes.fromhex(self.content[4:6]),byteorder="little"))
-
+        return SoftAPMode.map_obj(int.from_bytes(bytes.fromhex(self.content[4:6]), byteorder="little"))
 
     @property
-    def info(self) -> str|None:
+    def info(self) -> str | None:
         """info"""
         return self.content[6:] if len(self.content[6:]) > 0 else None
 
     def __str__(self) -> str:
         """str"""
-        return (f"{self.__class__.__name__}("
-                f"op_mode={self.op_mode}, "
-                f"soft_ap_status={self.soft_ap_status}, "
-                f"sta_status={self.sta_status}, "
-                f"info={self.info}, "
-                f"content={self.content})")
+        return (
+            f"{self.__class__.__name__}("
+            f"op_mode={self.op_mode}, "
+            f"soft_ap_status={self.soft_ap_status}, "
+            f"sta_status={self.sta_status}, "
+            f"info={self.info}, "
+            f"content={self.content})"
+        )
