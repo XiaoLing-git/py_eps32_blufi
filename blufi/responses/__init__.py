4c071bfeda37e24eb9f98891fe8a0bfa2897d96a
XiaoLing <samuel.ling.xiao@qq.com>
2025-09-24 10:27:04 +0800

    reformat response

--- blufi/responses/__init__.py
+++ blufi/responses/__init__.py
@@ -1,1 +1,19 @@
 """"""
+
+from typing import Union
+
+from .ack_parser import AckParser
+from .custom_data_parser import CustomDataParser
+from .error_parser import ErrorParser
+from .parser import Parser
+from .version_parser import VersionParser
+from .wifi_status_parser import WifiStatusParser
+
+Response_Parser_Type = Union[
+    AckParser,
+    CustomDataParser,
+    ErrorParser,
+    Parser,
+    VersionParser,
+    WifiStatusParser,
+]
